447e59277148d3b3e8e9dc1313fd3cd9b367fb5b
Gustavo Teixeira <10344032+gustavoteixeirah@users.noreply.github.com>
2025-08-01 02:54:30 -0300

    add username to /account endpoint

--- src/feature/Account/getAccount.js
+++ src/feature/Account/getAccount.js
@@ -1,10 +1,40 @@
 const Account = require("../../models/Account")
+const User = require("../../models/User")
+const userRepository = require("../../infra/mongoose/repository/userRepository")
 
 const getAccount = async ({
   filter, repository
 }) => {
   const result = await repository.get(filter)
-  return result?.map(user => new Account(user))
+  
+  if (!result || result.length === 0) {
+    return result
+  }
+
+  // Fetch user information for each account
+  const accountsWithUserInfo = await Promise.all(
+    result.map(async (accountData) => {
+      const account = new Account(accountData)
+      
+      // Fetch user data using userId from account
+      const userData = await userRepository.get({ _id: account.userId })
+      
+      if (userData && userData.length > 0) {
+        const user = new User(userData[0])
+        // Add user information to account
+        return {
+          ...account,
+          username: user.username,
+          email: user.email,
+          password: user.password
+        }
+      }
+      
+      return account
+    })
+  )
+  
+  return accountsWithUserInfo
 }
 
 module.exports = getAccount
